d32166471c4cf709040d31dad5368a038f4c8865
jiangTaoQuite <jiangtao103cp@163.com>
2017-04-08 23:37:50 +0800

    🐏

---
+++ Jquery/jQuery002/src/cn/itcast/servlet/ImageServlet.java
@@ -0,0 +1,86 @@
+package cn.itcast.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ImageServlet extends HttpServlet {
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
+		out.println("<HTML>");
+		out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
+		out.println("  <BODY>");
+		out.print("    This is ");
+		out.print(this.getClass());
+		out.println(", using the GET method");
+		out.println("  </BODY>");
+		out.println("</HTML>");
+		out.flush();
+		out.close();
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to post.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		System.out.println("itcast");
+		String imageType=request.getParameter("imageType");
+		if("1".equals(imageType)){
+			
+			out.println("image/1.jpg");
+		}
+		if("2".equals(imageType)){
+					
+			out.println("image/2.jpg");
+				}
+		if("3".equals(imageType)){
+			
+			out.println("image/3.jpg");
+		}
+		
+		if("4".equals(imageType)){
+			
+			out.println("image/4.jpg");
+		}
+		if("5".equals(imageType)){
+			out.println("image/5.jpg");
+			
+		}
+		if("6".equals(imageType)){
+			
+			out.println("image/6.jpg");
+		}
+		
+	}
+
+}
---
+++ Jquery/jQuery002/src/cn/itcast/servlet/LoadServlet.java
@@ -0,0 +1,51 @@
+package cn.itcast.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoadServlet extends HttpServlet {
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("get 方法被调用");
+		response.setContentType("text/html");
+		
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to post.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("post 方法被调用");
+		response.setContentType("text/html;charset=utf-8");
+		System.out.println(request.getParameter("username"));
+		PrintWriter out = response.getWriter();
+		
+		out.println("<div>您的认证已经成功！</div>");
+
+	}
+
+}
